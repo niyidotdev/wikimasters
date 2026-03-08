38befba96517e2e1453f85bd7e008ea8d33881af
niyi <akandefortunatus2021@gmail.com>
2026-03-08 16:43:04 +0100

    feat: Add new article button in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,9 +24,16 @@ export default async function Navbar() {
           <NavigationMenu>
             <NavigationMenuList className="flex items-center gap-2">
               {user ? (
-                <NavigationMenuItem>
-                  <UserButton />
-                </NavigationMenuItem>
+                <>
+                  <NavigationMenuItem>
+                    <Button asChild variant="outline">
+                      <Link href="/wiki/edit/new">New Article</Link>
+                    </Button>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <UserButton />
+                  </NavigationMenuItem>
+                </>
               ) : (
                 <>
                   <NavigationMenuItem>
